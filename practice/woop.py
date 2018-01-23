e7a44c513c835e6793a974acd3c345b24cf97086
George Jacobs <gj1_ferret@msn.com>
2018-01-23 19:14:29 +0000

    My django blog first commit

---
+++ practice/woop.py
@@ -0,0 +1,22 @@
+if 1 > 2:
+	print("Woop!")
+
+else:
+	print("Oop?")
+
+def hi():
+	print("Helloooooo")
+
+hi()
+
+name = "George"
+
+def hello(name):
+	print("Hi " + name)
+
+hello(name)
+
+names = ["Bob", "Benny", "Rhiannon"]
+
+for name in names:
+	hello(name)
