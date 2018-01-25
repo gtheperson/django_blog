a8fb1b4f8c1213b7a1c75b25c552050cf28ea942
George Jacobs <gj1_ferret@msn.com>
2018-01-25 19:23:46 +0000

    Set up initial view for site

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+# look here for blog url processing
+
+# get the url
+from django.conf.urls import url
+#import views from the currernt directory
+from . import views
+
+# url patterns just like one created in mysite dir, keeping the blog stuff here and redirecting from mysite url is neater
+urlpatterns = [
+	# this matches an empty string, which 127.0.0.8 etc is, and tells it to use a view named postlist for this url
+	# it also names the url, all urls should be given simple names
+	url(r'^$', views.post_list, name = 'post_list')
+]
--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    #redirects stuff will an empty url (like local server) to look for stuff in the blogs.url file
+    url(r'', include('blog.urls'))
 ]
